740897878012c336ab5ee9a89fac4dbd800f7cff
Kelvin Khoa <ttkhoa.hcmus@gmail.com>
2022-05-19 16:58:51 +0700

    fix fundVesting and addWhiteList function - related to problem: owner is msg.sender in ERC20

---
+++ hardhat.config.js
@@ -0,0 +1,68 @@
+require("@nomiclabs/hardhat-waffle");
+const { mnemonic } = require('./secrets.json');
+
+const ALCHEMY_API_KEY = "zvC3jUxjY4inMI26bKur2le4vCAKfs3K";
+const ROPSTEN_PRIVATE_KEY = "02edcda0d87b59864fe3e25f2124245bad67c724a65918cb0dcdb4994b85d7ac";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      gasPrice: 20000000000,
+      accounts: {mnemonic: mnemonic}
+    },
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
+  solidity: {
+  version: "0.8.0",
+  settings: {
+    optimizer: {
+      enabled: true
+    }
+   }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
+};
